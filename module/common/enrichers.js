f93278d78df181b19f9f630fa10c600789775c4c
khaali-dev <github@jgol.fr>
2023-08-28 22:42:14 +0200

    remplacement FAWS par text enrichers

--- module/common/enrichers.js
+++ module/common/enrichers.js
@@ -8,6 +8,23 @@ export default function setupTextEnrichers() {
                 awdoc.innerHTML = `<i class="fa ${match[1]}"></i>`;
                 return awdoc;
             }
+        },
+        {
+            pattern: /\@trinitesvie\[(.+?)\]{(.+?)}/gm,
+            enricher: async (match, options) => {
+                const viedoc = document.createElement("span");
+                let nbcases = parseInt(match[1]);
+                let nblignes = parseInt(match[2]);
+                let bulle = `<i class="far fa-circle"></i>`;
+                viedoc.className = "no-indent";
+                for(let i = 0; i < nblignes;i++){
+                    for(let i = 0; i < nbcases;i++){
+                        viedoc.innerHTML += bulle;
+                    }
+                    if(i+1<nblignes){viedoc.innerHTML += "<br />"};
+                }
+                return viedoc;
+            }
         }
     ]);
 }
